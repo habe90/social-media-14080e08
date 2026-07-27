c5b6dc1b82a415924c38bd391c6645607bdc562a
Halil Hodzic <habetech@gmail.com>
2026-07-28 00:44:38 +0200

    sync: src/js/modules/forum.js

--- src/js/modules/forum.js
+++ src/js/modules/forum.js
@@ -110,7 +110,7 @@ export function renderForumTopics(filterCategory = 'all') {
       btnUpvote.onclick = () => {
         topic.upvoted = !topic.upvoted;
         topic.upvotes += topic.upvoted ? 1 : -1;
-        if (topic.upvoted) playSound('like');
+        playSound(topic.upvoted ? 'like' : 'click');
         renderForumTopics(filterCategory);
       };
     }
@@ -119,6 +119,7 @@ export function renderForumTopics(filterCategory = 'all') {
     const repliesBox = card.querySelector('.forum-replies-box');
     if (btnReplies && repliesBox) {
       btnReplies.onclick = () => {
+        playSound('click');
         repliesBox.classList.toggle('hidden');
       };
     }
@@ -158,6 +159,7 @@ export function renderForumTopics(filterCategory = 'all') {
 function setupForumEvents() {
   document.querySelectorAll('.forum-chip').forEach(chip => {
     chip.onclick = () => {
+      playSound('click');
       document.querySelectorAll('.forum-chip').forEach(c => c.classList.remove('active'));
       chip.classList.add('active');
       const cat = chip.getAttribute('data-forum-cat');
@@ -168,6 +170,7 @@ function setupForumEvents() {
   const btnNewTopic = document.getElementById('btn-open-new-topic');
   if (btnNewTopic) {
     btnNewTopic.onclick = () => {
+      playSound('click');
       const modal = document.getElementById('new-topic-modal');
       if (modal) modal.classList.remove('hidden');
     };
